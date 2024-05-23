286a8eec2048000fdacf23ef32841aaef8117061
Robert Van der Klis <daniel.goodwin@student.uva.nl>
2024-05-23 15:30:37 +0200

    transformer fix in deployment attempt 5

--- util/skip_conf.py
+++ util/skip_conf.py
@@ -83,7 +83,7 @@ def get_skip_mask(
 
     conf_measure = get_confidence_class(key=key)    
 
-    if all_decoder_states:
+    if all_decoder_states is not None:
 
         conf = conf_measure(
             logits=logits,
