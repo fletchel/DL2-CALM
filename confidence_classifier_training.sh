ae34b5f6d0714c8c58272ea62a9f9754272a4d9c
Andrew Heath <31967107+andrewjh9@users.noreply.github.com>
2024-05-29 14:57:29 +0200

    Update run files for confidence_classifier.

--- confidence_classifier_training.sh
+++ confidence_classifier_training.sh
@@ -1,18 +1,19 @@
 python run_summarization.py \
-    --model_name_or_path ./save/cnndm_t5_small_run2/ \
-    --do_eval \
-    --dataset_name cnn_dailymail \
-    --dataset_config_name "3.0.0" \
-    --output_dir ./save/cnndm_t5_small_mlp\
-    --per_device_eval_batch_size 1 \
-    --deploy_scenario True \
-    --use_synchronize True \
-    --overwrite_output_dir \
-    --predict_with_generate \
-    --source_prefix "summarize: " \
-    --use_early_exit True \
-    --exit_conf_type MLP \
-    --exit_conf_threshold 0.6 \
-    --exit_min_layer 1 \
-    --max_eval_samples 1000 \
-    > outputs/output_confidence_classifier_training.log 2>&1
+  --model_name_or_path ./save/cnndm_t5_small_run2/ \
+  --do_train \
+  --dataset_name cnn_dailymail \
+  --dataset_config_name "3.0.0" \
+  --output_dir ./save/cnndm_t5_small_tmlp512_3epoch/ \
+  --per_device_train_batch_size 4 \
+  --overwrite_output_dir \
+  --predict_with_generate \
+  --source_prefix "summarize: " \
+  --do_eval \
+  --save_steps 100000000 \
+  --learning_rate 1e-4 \
+  --num_train_epochs 1 \
+  --train_meta_cm_head \
+   --output_hidden_states_decoder \
+  --max_eval_samples 1000 \
+  --exit_conf_type transformer_MLP_512
+  > outputs/output_confidence_classifier_training.log 2>&1
---
+++ confidence_classifier_training_eval.sh
@@ -0,0 +1,18 @@
+python run_summarization.py \
+    --model_name_or_path ./save/cnndm_t5_small_run2/ \
+    --do_eval \
+    --dataset_name cnn_dailymail \
+    --dataset_config_name "3.0.0" \
+    --output_dir ./save/cnndm_t5_small_mlp\
+    --per_device_eval_batch_size 1 \
+    --deploy_scenario True \
+    --use_synchronize True \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --source_prefix "summarize: " \
+    --use_early_exit True \
+    --exit_conf_type MLP \
+    --exit_conf_threshold 0.6 \
+    --exit_min_layer 1 \
+    --max_eval_samples 1000 \
+    > outputs/output_confidence_classifier_training_eval.log 2>&1
